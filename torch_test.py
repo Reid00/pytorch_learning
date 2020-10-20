dde87c8ec41cedb7302e9b9a92b49aab66a25e2d
reid <isreid.zhang@outlook.com>
2020-10-20 15:23:20 +0800

    update the first stage

--- torch_test.py
+++ torch_test.py
@@ -8,15 +8,20 @@
 '''
 
 import torch
-from torch import nn as nn
-from torch.utils.data import Dataset, DataLoader, TensorDataset
-from sklearn.model_selection import train_test_split
+from torch.utils.data import TensorDataset, Dataset, DataLoader
 
 
-a = [1,2,3]
-b = [4, 5, 6]
+BATCH_SIZE = 5
 
-x = DataLoader(TensorDataset(torch.tensor(a).float(), torch.tensor(b).float()))
-print(x)
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(10, 1, 10)
 
-train_test_split()
+print(x, y)
+
+torch_dataset = TensorDataset(x, y)
+
+dl = DataLoader(dataset=torch_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+
+for epoch in range(3):
+    for step, (batch_x, batch_y) in enumerate(dl):
+        print('EPOCH', epoch, '|STEP', step, '| batch x', batch_x.numpy(), '| batch y', batch_y.numpy())
